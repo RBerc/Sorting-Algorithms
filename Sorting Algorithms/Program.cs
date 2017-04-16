d8c1ce6b8fe70ece4ceb023cfec512577bbf5924
Robert Bercovic <rbercovic@gmail.com>
2017-04-16 19:32:34 -0400

    MergeSort Added

--- Sorting Algorithms/Program.cs
+++ Sorting Algorithms/Program.cs
@@ -8,6 +8,7 @@ namespace Sorting_Algorithms
 {
     class Program
     {
+        //Start of Insertion Sort
         public void insertionSort(int[] arr)
         {
             int num = arr.Length;
@@ -27,6 +28,7 @@ namespace Sorting_Algorithms
             }
         }
 
+        //Start of Selection Sort
         public void selectionSort(int[] arr)
         {
             int min, temp;
@@ -49,6 +51,8 @@ namespace Sorting_Algorithms
             }
             Console.WriteLine("Selection Sort Result: ", arr);
         }
+
+        //Start of Bubble Sort
         public void bubbleSort(int[] arr)
         {
 
@@ -68,6 +72,55 @@ namespace Sorting_Algorithms
             }
             Console.WriteLine("Bubble Sort Result: ", arr);
         }
+
+        //Start of MergeSort
+        public void merging( int[] arr, int left, int middle, int right)
+        {
+            int[] holder = new int[30];
+            int endLeft, num, holderPos;
+
+            endLeft = (middle - 1);
+            holderPos = left;
+            num = (right - left + 1);
+
+            while((left <= endLeft) && (middle <= right))
+            {
+                if (arr[left] <= arr[middle])
+                    holder[holderPos++] = arr[left++];
+                else
+                    holder[holderPos++] = arr[middle++];
+            }
+
+            while(left <= endLeft)
+            {
+                holder[holderPos++] = arr[left++];
+            }
+            while (middle <= right)
+            {
+                holder[holderPos++] = arr[middle++];
+            }
+
+            for(int i =0; i< num; i++)
+            {
+                arr[right] = holder[right];
+                right--;
+            }
+        }
+
+        public void MergeSort( int [] arr, int left, int right)
+        {
+            int middle;
+            if(right>left)
+            {
+                middle = (right + left) / 2;
+                MergeSort(arr, left, middle);
+                MergeSort(arr, (middle + 1), right);
+
+                merging(arr, left, (middle + 1), right); 
+            }
+            Console.WriteLine("Mergsort Result: ", arr);
+        }
+
 
         static void Main(string[] args)
         {
